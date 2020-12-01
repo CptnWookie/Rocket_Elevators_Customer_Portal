797258d2ebcd919ee34c29a121616dff0a68d4b6
Alexandre Leblanc <alexandreleblanc89@gmail.com>
2020-12-01 00:08:10 -0500

    starting to create login views. currently not working

--- Startup.cs
+++ Startup.cs
@@ -11,7 +11,6 @@ using Microsoft.Extensions.Hosting;
 
 namespace Rocket_Elevators_Customer_Portal
 {
-
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -21,35 +20,37 @@ namespace Rocket_Elevators_Customer_Portal
 
         public IConfiguration Configuration { get; }
 
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddControllersWithViews();
         }
 
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
 
-            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
