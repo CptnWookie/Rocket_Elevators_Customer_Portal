88c5d9a31ba09cabd6b947b037aeccb6b9ced3a0
Alexandre Leblanc <alexandreleblanc89@gmail.com>
2020-12-02 23:34:15 -0500

    intervention form template created

---
+++ wwwroot/js/api-ajax.js
@@ -0,0 +1,7 @@
+var xhr = new XMLHttpRequest();
+   xhr.onreadystatechange = function() {
+        var parsedResponse = JSON.parse(xhr.responseText);
+        document.getElementById('test').innerHTML = parsedResponse[0].author;
+   };
+   xhr.open('GET', 'https://rocketelevatorsfoundationrestapi.azurewebsites.net/api/Customers');
+   xhr.send();
